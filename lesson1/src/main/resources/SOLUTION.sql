c7c673eb2a85dd71a288c8b9e1905e1e724927c9
EDyugaeva <ed2408@yandex.ru>
2023-01-18 12:10:57 +0500

    commit

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,47 @@
+create table Student
+(
+    id       bigint PRIMARY KEY,
+    "name"     varchar,
+    birthday date,
+    "group"    integer
+
+);
+create table Subject
+(
+    id          bigint PRIMARY KEY,
+    "name"        varchar,
+    description varchar,
+    grade       int
+
+);
+
+create table PaymentType
+(
+    id   bigint primary key,
+    "name" varchar
+
+);
+
+create table Payment
+(
+    id           bigint Primary key,
+
+    type_id      bigint,
+    FOREIGN KEY (type_id) references PaymentType (id),
+    amount       decimal,
+    student_id   bigint,
+    FOREIGN KEY (student_id) references Student (id),
+    payment_date DateTime
+);
+
+create table Mark
+(
+    id         bigint Primary key,
+    student_id bigint,
+    FOREIGN KEY (student_id) references Student (id),
+    subject_id bigint,
+    FOREIGN KEY (subject_id) references Subject (id),
+    mark       int
+);
 
 
---
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -0,0 +1,20 @@
+alter table STUDENT
+    alter column BIRTHDAY drop not null;
+
+alter table MARK
+    ADD constraint mark_range check (MARK <= 10 AND MARK >= 1);
+alter table MARK
+    alter column SUBJECT_ID BIGINT not null;
+
+alter table SUBJECT
+    add constraint grade_range check (GRADE >= 1 AND GRADE <= 5);
+
+alter table PAYMENTTYPE
+    ADD UNIQUE (NAME);
+
+alter table PAYMENT
+    alter column TYPE_ID BIGINT not null;
+alter table PAYMENT
+    alter column AMOUNT NUMERIC not null;
+alter table PAYMENT
+    alter column PAYMENT_DATE TIMESTAMP not null;
