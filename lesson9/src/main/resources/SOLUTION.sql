4c7f2d30d14ce8c355beca95bf7e0689b55420ce
EDyugaeva <ed2408@yandex.ru>
2023-01-18 13:58:22 +0500

    commit

---
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -0,0 +1,3 @@
+select (STUDENT_ID) from MARK  group by STUDENT_ID having avg(MARK) > 8;
+select (STUDENT.ID, STUDENT.Name) from (MARK inner join STUDENT) group by STUDENT.NAME, STUDENT_ID having min(MARK) >7;
+select distinct (STUDENT.ID, STUDENT.Name) from (PAYMENT inner join STUDENT) where PAYMENT_DATE between '2019-01-01' and '2019-12-31'   group by STUDENT.NAME, STUDENT_ID having count(AMOUNT) >2 ;
