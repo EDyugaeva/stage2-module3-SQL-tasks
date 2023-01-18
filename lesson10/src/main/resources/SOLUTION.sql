a711824d01a7f7cce29898c07ec99211486da36c
EDyugaeva <ed2408@yandex.ru>
2023-01-18 21:49:35 +0500

    commit

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,1 +1,4 @@
-select (SUBJECT.ID, SUBJECT.Name, SUBJECT.DESCRIPTION, SUBJECT.GRADE) from SUBJECT inner join MARK group by SUBJECT.ID, SUBJECT.Name, SUBJECT.DESCRIPTION, SUBJECT.GRADE having avg(MARK) >8 ;
+select s.id, s.NAME, s.DESCRIPTION, s.GRADE from SUBJECT s inner join MARK M on s.ID = M.STUDENT_ID group by s.id having avg(m.MARK) > (select AVG(MARK) from MARK);
+
+select s.id, s.NAME, s.GROUPNUMBER, s.BIRTHDAY from STUDENT s inner join PAYMENT p on s.ID = p.STUDENT_ID group by s.id having avg(p.AMOUNT) > (select AVG(AMOUNT) from PAYMENT);
+
