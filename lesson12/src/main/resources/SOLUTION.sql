923337640fd2136a2b5289cdcc4958859e0a0762
EDyugaeva <ed2408@yandex.ru>
2023-01-19 12:21:08 +0500

    commit

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,7 @@
+delete from MARK where MARK <= 4;
+
+delete from PAYMENTTYPE where  NAME = 'DAILY';
 
 delete from MARK where MARK < 7;
 
-delete from PAYMENTTYPE where NAME = 'DAILY';
+select (s.ID, m.STUDENT_ID ) from SUBJECT s inner join MARK M on s.ID = M.SUBJECT_ID where GRADE <4;
--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,14 @@
-select * from student;
+alter table MARK
+    drop constraint mark_student_id_fkey;
+
+ALTER TABLE MARK
+    ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (STUDENT_ID) references STUDENT(id) ON DELETE CASCADE;
+
+
+
+alter table PAYMENT
+    drop constraint payment_type_id_fkey;
+
+ALTER TABLE PAYMENT
+ADD CONSTRAINT paymenttype_id_fkey FOREIGN KEY (type_id) references PAYMENTTYPE(id) ON DELETE CASCADE;
+
