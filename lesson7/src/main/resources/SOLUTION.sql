926a668c5e83bf40c184eb5fa90909642ae985a4
EDyugaeva <ed2408@yandex.ru>
2023-01-22 16:06:43 +0500

    commit

--- lesson7/src/main/resources/SOLUTION.sql
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -2,4 +2,4 @@ select * from Mark where mark > 6 order by mark desc;
 select * from Payment where Amount < 300 order by Amount asc;
 select * from PAYMENTTYPE order by NAME ;
 select * from STUDENT order by NAME desc;
-select DISTINCT s.NAME, s.GROUPNUMBER, s.ID,s.BIRTHDAY from STUDENT s inner join PAYMENT p on s.id = p.student_id group by s.birthday, s.id, s.name, s.groupnumber having min(amount) > 1000 order by s.BIRTHDAY;
+select DISTINCT s.NAME, s.GROUPNUMBER, s.ID,s.BIRTHDAY from STUDENT s inner join PAYMENT p on s.id = p.student_id where p.amount > 1000 order by s.BIRTHDAY;
